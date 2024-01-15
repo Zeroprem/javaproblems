cb21201414eee9964a9c63d2c1ce0cb033555094
prem tarange <88320933+Zeroprem@users.noreply.github.com>
2024-01-15 18:50:56 +0530

    Create arr.java

---
+++ arr.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class arr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows=sc.nextInt();
+        int cols=sc.nextInt();
+        int x=5;
+        int  [][] abc=new int [rows][cols];
+
+        for (int i=0;i<rows;i++){
+            for ( int j=0;j<cols;j++){
+                abc[i][j]=sc.nextInt();
+            }
+        }
+        for (int i=0;i<rows;i++){
+            for (int j=0;j<cols;j++){
+                System.out.print(abc[i][j] +" ");;
+            }
+            System.out.println();
+        }
+        for (int i=0;i<rows;i++){
+            for ( int j=0;j<cols;j++){
+                if (abc[i][j]==x){
+                    System.out.println(i+","+j);
+                }
+            }
+    }
+}}
