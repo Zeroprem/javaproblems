45b22dbf291e8607e35781f039e1cebeaba61819
prem tarange <88320933+Zeroprem@users.noreply.github.com>
2024-02-25 00:50:08 +0530

    Create bitManuplation.java

---
+++ bitManuplation.java
@@ -0,0 +1,46 @@
+public class bitManuplation {
+    public static int setBit(int n, int i){
+        int bitmask=1<<i;
+        return n|bitmask;
+
+    }
+  public static int clearBit(int n, int i){
+      int bitmask=~(1<<i);
+      return n&bitmask;
+
+  }
+  public static int updateBit(int n,int i, int newbit){
+//        if(newbit==0){
+//           return clearBit(n,i);
+//        }
+//        else {
+//            return setBit(n,i);
+//        }
+      n=clearBit(n,i);
+      int bitmask= newbit<<i;
+      return bitmask|n;
+  }
+
+  public static int clearIbits(int n , int i ){
+        int bimask=(~0)<<i;
+        return n&bimask;
+  }
+  public static int clearBitRange(int n,int i,int j){
+        int a=(~0)<<j+1;
+        int b=(1<<i)-1;
+        int bitmask =a|b;
+        return n&bitmask;
+  }
+    public static void main(String[] args) {
+        System.out.println(clearBitRange(20,1 ,3));
+
+
+//pring small a to z
+//        for(char cc='A';cc<='Z';cc++){
+//            System.out.println((char) (cc|' '));
+//        }
+//
+//
+
+    }
+}
